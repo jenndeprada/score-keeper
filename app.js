b37d5dd6056a7dc54f865f3a1cb19173bf759679
jenndeprada <jennifer.deprada@innovativedev.com>
2023-10-12 12:34:58 -0700

    Initialization

---
+++ app.js
@@ -0,0 +1,74 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+const p1ScoreDisplayed = document.querySelector('#p1Score');
+const p2ScoreDisplayed = document.querySelector('#p2Score');
+
+
+let p1ScoreTrack = 0;
+let p2ScoreTrack = 0;
+
+let winningScore = 5;
+let isGameOver = false;
+
+
+const playingTo = document.querySelector('#playingTo');
+playingTo.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+
+
+p1Button.addEventListener('click', function(){
+    if(!isGameOver){
+        p1ScoreTrack+=1;
+        
+        if(p1ScoreTrack === winningScore){
+            isGameOver = true;
+            p1ScoreDisplayed.classList.add('has-text-primary');
+            p2ScoreDisplayed.classList.add('has-text-danger');
+            
+        }
+        p1ScoreDisplayed.textContent = p1ScoreTrack;
+
+    }
+})
+
+
+p2Button.addEventListener('click', function(){
+    if(!isGameOver){
+        p2ScoreTrack+=1;
+        
+        if(p2ScoreTrack === winningScore){
+            isGameOver = true;
+            p2ScoreDisplayed.classList.add('has-text-primary');
+            p1ScoreDisplayed.classList.add('has-text-danger');
+        }
+        p2ScoreDisplayed.textContent = p2ScoreTrack;
+
+    }
+})
+
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', reset)
+
+
+
+function reset(){
+    isGameOver = false;
+    p1ScoreTrack = 0;
+    p2ScoreTrack = 0;
+
+    p1ScoreDisplayed.textContent = 0;
+    p2ScoreDisplayed.textContent = 0;
+
+    p2ScoreDisplayed.classList.remove('has-text-danger', 'has-text-primary');
+    p1ScoreDisplayed.classList.remove('has-text-primary', 'has-text-danger');
+
+}
+
+
+
+
